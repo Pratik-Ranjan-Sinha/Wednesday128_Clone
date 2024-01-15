de165e0429a80073f4c3b189e60ae048fe55010f
Pratik Ranjan <pratikranjansinha@gmail.com>
2024-01-16 01:15:52 +0530

    quar commit

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ tl.from("#nav h1, #nav2 h4, #nav3 i, #nav3 h4 ",{
     stagger:0.05,
     // delay: 0.5
 })
+gsap.to("#page1 #img1,#page1 #img2,#page1 #img3,#page1 #img4,#page1 #img5",{
+    y: 50,
+    delay: 1,
+    duration: 3,
+    repeat: -1,
+    yoyo: true
+})
 tl.from("#center h1",{
     y: 30,
     scale: 1.6,
@@ -20,4 +27,17 @@ tl.from("#page1 img",{
     scale: 0.8,
     stagger:0.02,
     opacity:0
+})
+
+var tl2 = gsap.timeline()
+
+tl2.from("#main #page2 h1",{
+    y: 30,
+    scale: 1.6,
+    opacity: 0,
+    stagger: 0.3
+})
+tl2.from("#main #page2 p",{
+    opacity:0,
+    stagger: 0.01
 })
